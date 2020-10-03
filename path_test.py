4260e79164abd17326fec5a9b6e7af05ef53556f
Tiffany <tiffanyec@berkeley.edu>
2020-10-03 12:46:15 -0700

    cleaned up path_test.py

--- path_test.py
+++ path_test.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 """
-Path Planning Script for Lab 7
-Author: Valmik Prabhu
+Path Planning Script for Lab 5
 """
 import sys
-assert sys.argv[1] in ("sawyer", "baxter")
-ROBOT = sys.argv[1]
 
-if ROBOT == "baxter":
-    from baxter_interface import Limb
-else:
-    from intera_interface import Limb
+from baxter_interface import Limb
 
 import rospy
 import numpy as np
@@ -21,8 +15,36 @@ from geometry_msgs.msg import PoseStamped
 
 from path_planner import PathPlanner
 
-# Uncomment this line for 5 of Lab 5
+# Uncomment this line for part 5 of Lab 5
 # from controller import Controller
+
+def move_to_goal(x, y, z, orien_const=[], or_x=0.0, or_y=-1.0, or_z=0.0, or_w=0.0):
+	try:
+	    goal = PoseStamped()
+	    goal.header.frame_id = "base"
+
+	    #x, y, and z position
+	    goal.pose.position.x = x
+	    goal.pose.position.y = y
+	    goal.pose.position.z = z
+
+	    #Orientation as a quaternion
+	    goal.pose.orientation.x = or_x
+	    goal.pose.orientation.y = or_y
+	    goal.pose.orientation.z = or_z
+	    goal.pose.orientation.w = or_w
+
+	    plan = planner.plan_to_pose(goal, orien_const)
+
+	    raw_input("Press <Enter> to move the right arm to goal pose: ")
+
+	    # Might have to edit this for part 5
+	    if not planner.execute_plan(plan):
+	        raise Exception("Execution failed")
+	except Exception as e:
+	    print e
+	    traceback.print_exc()
+
 
 def main():
     """
@@ -33,21 +55,15 @@ def main():
 
     planner = PathPlanner("right_arm")
 
-    if ROBOT == "sawyer":
-        Kp = 0.2 * np.array([0.4, 2, 1.7, 1.5, 2, 2, 3])
-        Kd = 0.01 * np.array([2, 1, 2, 0.5, 0.8, 0.8, 0.8])
-        Ki = 0.01 * np.array([1.4, 1.4, 1.4, 1, 0.6, 0.6, 0.6])
-        Kw = np.array([0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9])
-    else:
-        Kp = 0.45 * np.array([0.8, 2.5, 1.7, 2.2, 2.4, 3, 4])
-        Kd = 0.015 * np.array([2, 1, 2, 0.5, 0.8, 0.8, 0.8])
-        Ki = 0.01 * np.array([1.4, 1.4, 1.4, 1, 0.6, 0.6, 0.6])
-        Kw = np.array([0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9])
+    # K values for Part 5
+    Kp = 0.1 * np.array([0.3, 2, 1, 1.5, 2, 2, 3]) # Borrowed from 106B Students
+    Kd = 0.01 * np.array([2, 1, 2, 0.5, 0.5, 0.5, 0.5]) # Borrowed from 106B Students
+    Ki = 0.01 * np.array([1, 1, 1, 1, 1, 1, 1]) # Untuned
+    Kw = np.array([0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9]) # Untuned
 
-
-    ##
-    ## Add the obstacle to the planning scene here
-    ##
+    #-----------------------------------------------------#
+    ## Add any obstacles to the planning scene here
+    #-----------------------------------------------------#
 
     # #Create a path constraint for the arm
     # #UNCOMMENT FOR THE ORIENTATION CONSTRAINTS PART
@@ -63,88 +79,11 @@ def main():
     while not rospy.is_shutdown():
 
         while not rospy.is_shutdown():
-            try:
-                if ROBOT == "baxter":
-                    x, y, z = 0.47, -0.85, 0.07
-                else:
-                    x, y, z = 0.8, 0.05, -0.23
-                goal_1 = PoseStamped()
-                goal_1.header.frame_id = "base"
+        	move_to_goal(0.47, -0.85, 0.07)
+            move_to_goal(0.6, -0.3, 0.0)
+            move_to_goal(0.6, -0.1, 0.1)
 
-                #x, y, and z position
-                goal_1.pose.position.x = x
-                goal_1.pose.position.y = y
-                goal_1.pose.position.z = z
-
-                #Orientation as a quaternion
-                goal_1.pose.orientation.x = 0.0
-                goal_1.pose.orientation.y = -1.0
-                goal_1.pose.orientation.z = 0.0
-                goal_1.pose.orientation.w = 0.0
-
-                # Might have to edit this . . . 
-                plan = planner.plan_to_pose(goal_1, [])
-
-                raw_input("Press <Enter> to move the right arm to goal pose 1: ")
-                if not planner.execute_plan(plan):
-                    raise Exception("Execution failed")
-            except Exception as e:
-                print e
-                traceback.print_exc()
-            else:
-                break
-
-        while not rospy.is_shutdown():
-            try:
-                goal_2 = PoseStamped()
-                goal_2.header.frame_id = "base"
-
-                #x, y, and z position
-                goal_2.pose.position.x = 0.6
-                goal_2.pose.position.y = -0.3
-                goal_2.pose.position.z = 0.0
-
-                #Orientation as a quaternion
-                goal_2.pose.orientation.x = 0.0
-                goal_2.pose.orientation.y = -1.0
-                goal_2.pose.orientation.z = 0.0
-                goal_2.pose.orientation.w = 0.0
-
-                plan = planner.plan_to_pose(goal_2, [])
-
-                raw_input("Press <Enter> to move the right arm to goal pose 2: ")
-                if not planner.execute_plan(plan):
-                    raise Exception("Execution failed")
-            except Exception as e:
-                print e
-            else:
-                break
-
-        while not rospy.is_shutdown():
-            try:
-                goal_3 = PoseStamped()
-                goal_3.header.frame_id = "base"
-
-                #x, y, and z position
-                goal_3.pose.position.x = 0.6
-                goal_3.pose.position.y = -0.1
-                goal_3.pose.position.z = 0.1
-
-                #Orientation as a quaternion
-                goal_3.pose.orientation.x = 0.0
-                goal_3.pose.orientation.y = -1.0
-                goal_3.pose.orientation.z = 0.0
-                goal_3.pose.orientation.w = 0.0
-
-                plan = planner.plan_to_pose(goal_3, [])
-
-                raw_input("Press <Enter> to move the right arm to goal pose 3: ")
-                if not planner.execute_plan(plan):
-                    raise Exception("Execution failed")
-            except Exception as e:
-                print e
-            else:
-                break
+        
 
 if __name__ == '__main__':
     rospy.init_node('moveit_node')
