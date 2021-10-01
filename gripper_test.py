832ab20aa8ccbe9fe4b69db998921ac1291fc45f
josephinekoe <koejosephine@gmail.com>
2021-10-01 13:29:22 -0700

    editing comments and gripper_test

--- gripper_test.py
+++ gripper_test.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 import rospy
-from baxter_interface import gripper as robot_gripper
-#from intera_interface import gripper as robot_gripper
+import sys
+
+if sys.argv[1] == 'sawyer':
+	from intera_interface import gripper as robot_gripper
+else:
+    from baxter_interface import gripper as robot_gripper
 
 rospy.init_node('gripper_test')
 
-#Set up the right gripper
+# Set up the right gripper
 right_gripper = robot_gripper.Gripper('right')
 
-#Calibrate the gripper (other commands won't work unless you do this first)
+# Calibrate the gripper (other commands won't work unless you do this first)
 print('Calibrating...')
 right_gripper.calibrate()
 rospy.sleep(2.0)
 
-#Close the right gripper
+# Close the right gripper
 print('Closing...')
 right_gripper.close()
 rospy.sleep(1.0)
 
-#Open the right gripper
+# Open the right gripper
 print('Opening...')
 right_gripper.open()
 rospy.sleep(1.0)
--- ik_example.py
+++ ik_example.py
@@ -8,22 +8,22 @@ from numpy import linalg
 import sys
 
 def main(robo):
-    #Wait for the IK service to become available
+    # Wait for the IK service to become available
     rospy.wait_for_service('compute_ik')
     rospy.init_node('service_query')
     arm = 'left'
-    #Create the function used to call the service
+    # Create the function used to call the service
     compute_ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
     if robo == 'sawyer':
     	arm = 'right'
     while not rospy.is_shutdown():
         raw_input('Press [ Enter ]: ')
         
-        #Construct the request
+        # Construct the request
         request = GetPositionIKRequest()
         request.ik_request.group_name = arm + "_arm"
 
-        #Alan does not have a gripper so replace link with 'right_wrist' instead
+        # If a Sawyer does not have a gripper, replace '_gripper_tip' with '_wrist' instead
         link = arm + "_gripper"
         if robo == 'sawyer':
         	link += '_tip'
@@ -32,7 +32,7 @@ def main(robo):
         request.ik_request.attempts = 20
         request.ik_request.pose_stamped.header.frame_id = "base"
         
-        #Set the desired orientation for the end effector HERE
+        # Set the desired orientation for the end effector HERE
         request.ik_request.pose_stamped.pose.position.x = 0.5
         request.ik_request.pose_stamped.pose.position.y = 0.5
         request.ik_request.pose_stamped.pose.position.z = 0.0        
@@ -42,10 +42,10 @@ def main(robo):
         request.ik_request.pose_stamped.pose.orientation.w = 0.0
         
         try:
-            #Send the request to the service
+            # Send the request to the service
             response = compute_ik(request)
             
-            #Print the response HERE
+            # Print the response HERE
             print(response)
             group = MoveGroupCommander(arm + "_arm")
 
@@ -62,7 +62,7 @@ def main(robo):
         except rospy.ServiceException, e:
             print "Service call failed: %s"%e
 
-#Python's syntax for a main() method
+# Python's syntax for a main() method
 if __name__ == '__main__':
     main(sys.argv[1])
 
